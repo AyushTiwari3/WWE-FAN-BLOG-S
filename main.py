fefaf6ccde4b5379afca56413eab3b418280ea26
AyushTiwari3 <tiwariayush222@gmail.com>
2023-07-07 23:43:19 +0530

    hui hui

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort,request 
-from flask_bootstrap import Bootstrap4
+from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
 from functools import wraps
@@ -16,7 +16,7 @@ R_EMAIL="tiwariayush222@gmail.com"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
-bootsrtap=Bootstrap4(app)
+bootsrtap=Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wwe.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
