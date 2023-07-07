6e7dd30b552edd08fd24ac3403698688dee6d7bb
AyushTiwari3 <tiwariayush222@gmail.com>
2023-07-07 23:54:40 +0530

    .

--- main.py
+++ main.py
@@ -227,6 +227,5 @@ def logout():
     return redirect(url_for('get_all_posts'))
 
 
-if __name__=="__main__":
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
