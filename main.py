4a177ac8a159844341095dc86ea2989055fc7215
AyushTiwari3 <tiwariayush222@gmail.com>
2023-07-08 16:00:55 +0530

    no change

--- main.py
+++ main.py
@@ -234,7 +234,5 @@ def logout():
     return redirect(url_for('get_all_posts'))
 
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
